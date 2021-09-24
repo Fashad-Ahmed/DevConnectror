efa15892b89ab8a3b995793b56dfbecca969f4d8
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-09-25 01:23:03 +0500

    undefined issue dealt

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from  './reducers';
+import rootReducer from './reducers';
+import setAuthToken from './utils/setAuthToken';
 
 const initialState = {};
 
@@ -20,15 +21,15 @@ const store = createStore(
 // preventing undefined error
 let currentState = store.getState();
 
-// store.subscribe(() => {
-//   // keep track of the previous and current state to compare changes
-//   let previousState = currentState;
-//   currentState = store.getState();
-//   // if the token changes set the value in localStorage and axios headers
-//   if (previousState.auth.token !== currentState.auth.token) {
-//     const token = currentState.auth.token;
-//     setAuthToken(token);
-//   }
-// });
+store.subscribe(() => {
+    // keep track of the previous and current state to compare changes
+    let previousState = currentState;
+    currentState = store.getState();
+    // if the token changes set the value in localStorage and axios headers
+    if (previousState.auth.token !== currentState.auth.token) {
+        const token = currentState.auth.token;
+        setAuthToken(token);
+    }
+});
 
 export default store;
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import store from '../store';
+import store from '../store';
 import { LOGOUT } from '../actions/types';
 
 const api = axios.create({
@@ -8,6 +8,7 @@ const api = axios.create({
     'Content-Type': 'application/json'
   }
 });
+
 /**
  intercept any error responses from the api
  and check if the token is no longer valid.
@@ -16,14 +17,14 @@ const api = axios.create({
  logout the user if the token has expired
 **/
 
-// api.interceptors.response.use(
-//   res => res,
-//   err => {
-//     if (err.response.status === 401) {
-//       store.dispatch({ type: LOGOUT });
-//     }
-//     return Promise.reject(err);
-//   }
-// );
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response.status === 401) {
+      store.dispatch({ type: LOGOUT });
+    }
+    return Promise.reject(err);
+  }
+);
 
 export default api;
